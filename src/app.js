18bfcdb9a0e2c0ad1fc0cacabfd954bf8b14dd25
Júlia Leal <julia.borges201@gmail.com>
2026-03-11 14:11:22 -0300

    feat(app): cria app.js com configuração do Express e middlewares base

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ erro: "Erro interno do servidor" });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
